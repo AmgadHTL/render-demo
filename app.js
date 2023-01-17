d93ca23fceabe227bca87e47d06cc74b977ca093
Amgad Darwish <darwish.a03@htlwienwest.at>
2023-01-17 17:54:35 +0100

    initial version

--- app.js
+++ app.js
@@ -4,20 +4,20 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import path from 'path';
 import cors from 'cors';
-import  immoRouter  from "./routes/immo.js";
-
+import immoRouter from './routes/immo.js';
 
 dotenv.config();
 const app = express();
-
-
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  }),
+);
 app.use(morgan('tiny'));
 app.use(express.json());
 const dirname = path.resolve();
 app.use(express.static(path.join(dirname, 'public')));
 app.use('/', immoRouter);
-
-
 
 app.get('*', (req, res) => res.status(404).sendFile(path.join(dirname, 'error/404-error.webp')));
 
